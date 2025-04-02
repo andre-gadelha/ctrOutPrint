26ae61e9fda81324d658b29b373d7e06bd78b3d0
andre-gadelha <adrrda@gmail.com>
2025-04-02 16:20:37 -0300

    Commit Inicial

---
+++ src/main/java/sef/CtrOutPrint/api_CtrOutPrint/ApiCtrOutPrintApplication.java
@@ -0,0 +1,13 @@
+package sef.CtrOutPrint.api_CtrOutPrint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCtrOutPrintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCtrOutPrintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sef/CtrOutPrint/api_CtrOutPrint/ApiCtrOutPrintApplicationTests.java
@@ -0,0 +1,13 @@
+package sef.CtrOutPrint.api_CtrOutPrint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCtrOutPrintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
